b07ec905c7f3225768cd184f149114f1aebc36a9
AGI Corp <agicorp.eth@icloud.com>
2026-03-27 17:36:55 -0700

    Add EpochTracker, EvolutionReporter, and NANDABridge

--- evolution/__init__.py
+++ evolution/__init__.py
@@ -6,6 +6,9 @@ from .sandbox import Sandbox
 from .engine import EvolutionEngine
 from .version_control import GitManager
 from .supervisor import Supervisor
+from .epoch_tracker import EpochTracker
+from .reporting import EvolutionReporter
+from .nanda_bridge import NANDABridge, setup_nanda
 
 __all__ = [
     "ObserverAgent",
@@ -16,4 +19,8 @@ __all__ = [
     "EvolutionEngine",
     "GitManager",
     "Supervisor",
+    "EpochTracker",
+    "EvolutionReporter",
+    "NANDABridge",
+    "setup_nanda",
 ]
